643e26b04a3c20efd6778a69fbd172d2e1ff8389
Stefan Berggren <stefan.berggren@gmail.com>
2014-01-02 20:10:52 +0100

    Help pages for modchan.

--- src/me/odium/simplechatchannels/commands/scc.java
+++ src/me/odium/simplechatchannels/commands/scc.java
@@ -40,6 +40,7 @@ public class scc implements CommandExecutor {
       sender.sendMessage(ChatColor.GREEN + " /delchan <channel> " + ChatColor.WHITE + "- Delete a channel you own");
       sender.sendMessage(ChatColor.GREEN + " /joinchan <channel> " + ChatColor.WHITE + "- Join or set channel as active");
       sender.sendMessage(ChatColor.GREEN + " /leavechan <channel> " + ChatColor.WHITE + "- Leave a channel");
+      sender.sendMessage(ChatColor.GREEN + " /modchan <channel> <param> " + ChatColor.WHITE + "- Modify a channel, type /scc help modchan for help");
       sender.sendMessage(ChatColor.GREEN + " /global " + ChatColor.WHITE + "- Return to global chat");
       sender.sendMessage(ChatColor.GREEN + " /topic <channel> <topic> " + ChatColor.WHITE + "- Set a channel topic");
       if (sender.hasPermission("scc.spychan")) {
@@ -65,6 +66,12 @@ public class scc implements CommandExecutor {
       sender.sendMessage(ChatColor.GREEN + " /msg " + ChatColor.WHITE + "- Return to channel/global chat");
       sender.sendMessage(ChatColor.GREEN + " /msg <player> " + ChatColor.WHITE + "- Lock all messages to player");
       sender.sendMessage(ChatColor.GREEN + " /msg <player> my message " + ChatColor.WHITE + "- Send a message to <player>");
+      return true;      
+    } else if (args.length == 2 && args[0].equalsIgnoreCase("help") && args[1].equalsIgnoreCase("modchan")) {
+      sender.sendMessage(ChatColor.GOLD + "[ "+ChatColor.WHITE+"Modchan Commands"+ChatColor.GOLD+" ]");
+      sender.sendMessage(ChatColor.GREEN + " /modchan <channel> lock " + ChatColor.WHITE + "- Lock channel");
+      sender.sendMessage(ChatColor.GREEN + " /modchan <channel> unlock " + ChatColor.WHITE + "- Unlock channel");
+      sender.sendMessage(ChatColor.WHITE + " See /scc help channels for info how to add/remove players to the access list.");
       return true;      
     } else if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
       if(player == null || player.hasPermission("scc.reload")) {
